5ae6b59c0680a1c59bfbd4c3bc218eaa33ffba2b
anushkadeshpande <anushkaa5000@gmail.com>
2023-10-09 19:34:42 +0530

    feat: configured Hikari connection pool

---
+++ src/main/java/com/amigoscode/config/DatasourceConfig.java
@@ -0,0 +1,20 @@
+package com.amigoscode.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+public class DatasourceConfig {
+    
+    @Bean
+    @Primary
+    @ConfigurationProperties("app.datasource.main")
+    public HikariDataSource hikariDataSource() {
+        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    }
+}
