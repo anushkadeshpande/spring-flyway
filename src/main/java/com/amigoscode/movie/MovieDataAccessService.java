666e9b082857144e224dff038ac5864697c1afec
anushkadeshpande <anushkaa5000@gmail.com>
2023-10-09 20:24:39 +0530

    feat: added implementation for selectMovies

--- src/main/java/com/amigoscode/movie/MovieDataAccessService.java
+++ src/main/java/com/amigoscode/movie/MovieDataAccessService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,7 +19,18 @@ public class MovieDataAccessService implements MovieDao {
 
     @Override
     public List<Movie> selectMovies() {
-        throw new UnsupportedOperationException("not implemented");
+        // throw new UnsupportedOperationException("not implemented");
+        String sql = """
+                SELECT id, name, release_date 
+                FROM movie
+                LIMIT 10;
+                """;
+        return jdbcTemplate.query(sql, (resultSet, i) -> {
+            return new Movie(resultSet.getInt("id"),
+            resultSet.getString("name"),
+            null,
+            LocalDate.parse(resultSet.getString("release_date")));
+        });
     }
 
     @Override
